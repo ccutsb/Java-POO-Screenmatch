65e10cb911e51e0b2b6c8510e9cb1541e8aa210b
ccutsb <cristiancortes9297@gmail.com>
2024-12-13 09:10:41 -0300

    Add: Implementa la interfaz de Clasificación en la clase Película
    Creación de la interfaz Clasificación con el método getClasificacion()
    Implementación de la interfaz Clasificación en la clase Película
    Implementación del método getClasificacion() en Película, calculando la clasificación en estrellas a partir de la media de las evaluaciones
    Eliminación de la relación de herencia entre Película y Título, ya que no es apropiada
    Uso de interfaces para implementar comportamientos comunes entre clases sin necesidad de herencia.

--- src/com/aluracursos/screenmatch/modelos/Pelicula.java
+++ src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -1,13 +1,17 @@
 package com.aluracursos.screenmatch.modelos;
 
-public class Pelicula extends Titulo {
+import com.aluracursos.screenmatch.calculos.Clasificable;
+
+public class Pelicula extends Titulo implements Clasificable{
+
     private String director;
 
-    public String getDirector() {
-        return director;
-    }
+    public String getDirector() {return director;}
 
-    public void setDirector(String director) {
-        this.director = director;
+    public void setDirector(String director) {this.director = director;}
+
+    @Override
+    public int getClasificable() {
+        return (int) (calculaMedia() / 2);
     }
 }
